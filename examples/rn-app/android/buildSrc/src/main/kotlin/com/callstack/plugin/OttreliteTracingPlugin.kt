2eb140228b66271c4d151d7411d60f3d46dcb6d9
artus9033 <artus9033@gmail.com>
2025-10-25 15:56:27 +0200

    feat: added Gradle plugin config to filter variants that should have RN internals instrumentation

--- examples/rn-app/android/buildSrc/src/main/kotlin/com/callstack/plugin/OttreliteTracingPlugin.kt
+++ examples/rn-app/android/buildSrc/src/main/kotlin/com/callstack/plugin/OttreliteTracingPlugin.kt
@@ -5,22 +5,68 @@ import com.android.build.api.instrumentation.InstrumentationScope
 import com.android.build.api.variant.AndroidComponentsExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.provider.SetProperty
+
+open class OttreliteTracingExtension(project: Project) {
+    /**
+     * Set of variant names where tracing should be enabled.
+     * If empty, tracing will be applied to all variants.
+     *
+     * Example usage in build.gradle:
+     * ```
+     * ottreliteTracing {
+     *     rnInstrumentationVariants = ["debug", "profiling"]
+     * }
+     * ```
+     */
+    val rnInstrumentationVariants: SetProperty<String> =
+        project.objects.setProperty(String::class.java)
+            .convention(emptySet())
+}
 
 class OttreliteTracingPlugin : Plugin<Project> {
     override fun apply(project: Project) {
+        val extension = project.extensions.create(
+            "ottreliteTracing",
+            OttreliteTracingExtension::class.java,
+            project
+        )
+
         val androidComponents = project.extensions.getByType(AndroidComponentsExtension::class.java)
 
         androidComponents.onVariants { variant ->
-            variant.instrumentation.transformClassesWith(
-                OttreliteClassVisitorFactory::class.java,
-                InstrumentationScope.ALL
-            ) { params ->
-                params.targetClassName.set("com.facebook.systrace.Systrace")
-                params.timestamp.set(System.currentTimeMillis())
+            val variantName = variant.name
+            val shouldInstrument = shouldInstrumentVariant(variantName, extension)
+            if (shouldInstrument) {
+                project.logger.lifecycle("OttreliteTracing: Enabling instrumentation for variant '$variantName'")
+
+                variant.instrumentation.transformClassesWith(
+                    OttreliteClassVisitorFactory::class.java,
+                    InstrumentationScope.ALL
+                ) { params ->
+                    params.targetClassName.set("com.facebook.systrace.Systrace")
+                    params.timestamp.set(System.currentTimeMillis())
+                }
+                variant.instrumentation.setAsmFramesComputationMode(
+                    FramesComputationMode.COMPUTE_FRAMES_FOR_INSTRUMENTED_METHODS
+                )
+            } else {
+                project.logger.lifecycle("OttreliteTracing: Skipping instrumentation for variant '$variantName'")
             }
-            variant.instrumentation.setAsmFramesComputationMode(
-                FramesComputationMode.COMPUTE_FRAMES_FOR_INSTRUMENTED_METHODS
-            )
         }
+    }
+
+    private fun shouldInstrumentVariant(
+        variantName: String,
+        extension: OttreliteTracingExtension
+    ): Boolean {
+        val rnInstrumentationVariants = extension.rnInstrumentationVariants.get()
+        // if enabled variants are specified, only instrument those
+        if (rnInstrumentationVariants.isNotEmpty()) {
+            return rnInstrumentationVariants.contains(variantName)
+        }
+
+        // otherwise, instrument all variants
+        return true
     }
 }
--- packages/backend-platform/src/index.ts
+++ packages/backend-platform/src/index.ts
@@ -3,17 +3,21 @@ import { NitroModules } from 'react-native-nitro-modules';
 
 import type { ReactNativeOttreliteBackendPlatform } from './specs/ReactNativeOttreliteBackendPlatform.nitro';
 
-const ReactNativeOttreliteBackendPlatformHybridObject =
-  NitroModules.createHybridObject<ReactNativeOttreliteBackendPlatform>(
-    'ReactNativeOttreliteBackendPlatform'
-  );
-
 /**
  * Ottrelite backend providing integration with the OS-specific tracing API available at runtime.
  * On Android, it uses ATrace, on iOS, it uses OSSignposter.
  */
 export class OttreliteBackendPlatform {
+  static hybridObject: ReactNativeOttreliteBackendPlatform;
+
   static install() {
-    ReactNativeOttreliteBackendPlatformHybridObject.install();
+    if (!this.hybridObject) {
+      this.hybridObject =
+        NitroModules.createHybridObject<ReactNativeOttreliteBackendPlatform>(
+          'ReactNativeOttreliteBackendPlatform'
+        );
+    }
+
+    this.hybridObject.install();
   }
 }
